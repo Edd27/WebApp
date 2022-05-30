37f78f13ccf195aae1122567995b3546409535ae
Edgar Benavides <edgarben27@gmail.com>
2022-05-29 22:42:46 -0500

    update functions on Tools.tsx

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -11,14 +11,44 @@ import './Tools.css'
 import { Clipboard } from '@capacitor/clipboard'
 import { Toast } from '@capacitor/toast'
 import { Share } from '@capacitor/share'
-import { ActionSheet, ActionSheetButtonStyle } from '@capacitor/action-sheet'
 import { Device } from '@capacitor/device'
 import { Network } from '@capacitor/network'
+import { ActionSheet } from '@capacitor/action-sheet'
 
 interface ToolsProps {}
 
 const Tools: React.FC<ToolsProps> = () => {
-  const writeToClipboard = async () => {
+  const handleActionSheet = async () => {
+    await ActionSheet.showActions({
+      title: 'What would you like to do?',
+      message: 'Select an action',
+      options: [
+        {
+          title: 'Option 1'
+        },
+        {
+          title: 'Option 2'
+        },
+        {
+          title: 'Option 3'
+        },
+        {
+          title: 'Option 4'
+        }
+      ]
+    })
+  }
+
+  const handleShare = async () => {
+    await Share.share({
+      title: 'Shared content from Ionic App',
+      text: 'WebApp Ionic and Capacitor',
+      url: 'https://ionicframework.com',
+      dialogTitle: 'Share Ionic Framework'
+    })
+  }
+
+  const handleClipboard = async () => {
     await Clipboard.write({
       string: 'Hello World!'
     })
@@ -27,62 +57,30 @@ const Tools: React.FC<ToolsProps> = () => {
       text: 'Copied to clipboard'
     })
   }
-  const showToast = async () => {
+
+  const handleToast = async () => {
     await Toast.show({
       text: 'This is a toast!'
     })
   }
-  const showActions = async () => {
-    const result = await ActionSheet.showActions({
-      title: 'Photo Options',
-      message: 'Select an option to perform',
-      options: [
-        {
-          title: 'Upload'
-        },
-        {
-          title: 'Share'
-        },
-        {
-          title: 'Remove',
-          style: ActionSheetButtonStyle.Destructive
-        }
-      ]
-    })
 
-    await Toast.show({
-      text: 'Action Sheet result:' + result
-    })
-  }
-
-  const showShare = async () => {
-    const result = await Share.share({
-      title: 'See cool stuff',
-      text: 'Really awesome thing you need to see right meow',
-      url: 'http://ionicframework.com/',
-      dialogTitle: 'Share with buddies'
-    })
-    await Toast.show({
-      text: result.toString()
-    })
-  }
-
-  const logBatteryInfo = async () => {
+  const handleBatteryInfo = async () => {
     const info = await Device.getBatteryInfo()
 
     await Toast.show({
-      text: info.toString()
+      text: `Level: ${info.batteryLevel?.toString()} \nState: ${
+        info.isCharging ? 'Charging' : 'No Charging'
+      }`
     })
   }
 
-  Network.addListener('networkStatusChange', status => {
-    console.log('Network status changed', status)
-  })
-
-  const logCurrentNetworkStatus = async () => {
+  const handleNetworkInfo = async () => {
     const status = await Network.getStatus()
+    const { connected, connectionType } = status
     await Toast.show({
-      text: 'Network status:' + status
+      text: `Status: ${
+        connected ? 'Connected' : 'Disconnected'
+      } \nType: ${connectionType}`
     })
   }
 
@@ -93,38 +91,38 @@ const Tools: React.FC<ToolsProps> = () => {
         <IonIcon
           slot='icon-only'
           icon={wifi}
-          onClick={() => logCurrentNetworkStatus()}
+          onClick={handleNetworkInfo}
         ></IonIcon>
       </IonButton>
       <IonButton className='btn-tools'>
         <IonIcon
           slot='icon-only'
           icon={batteryFull}
-          onClick={() => logBatteryInfo()}
+          onClick={handleBatteryInfo}
         ></IonIcon>
       </IonButton>
       <IonButton className='btn-tools'>
         <IonIcon
           slot='icon-only'
           icon={shareSocial}
-          onClick={() => showShare()}
+          onClick={handleShare}
         ></IonIcon>
       </IonButton>
       <IonButton className='btn-tools'>
         <IonIcon
           slot='icon-only'
           icon={notifications}
-          onClick={() => showToast()}
+          onClick={handleToast}
         ></IonIcon>
       </IonButton>
       <IonButton className='btn-tools'>
         <IonIcon
           slot='icon-only'
           icon={menu}
-          onClick={() => showActions()}
+          onClick={handleActionSheet}
         ></IonIcon>
       </IonButton>
-      <IonButton className='btn-tools' onClick={() => writeToClipboard()}>
+      <IonButton className='btn-tools' onClick={handleClipboard}>
         <IonIcon slot='icon-only' icon={clipboard}></IonIcon>
       </IonButton>
     </IonList>
--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,6 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-defineCustomElements(window)
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
@@ -23,3 +21,5 @@ serviceWorkerRegistration.unregister()
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
+
+defineCustomElements(window)
